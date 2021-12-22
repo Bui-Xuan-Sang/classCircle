18915cd09234ddbdeb416fdf792c1f876f2ad56f
unknown <sangnhanma14@gmail.com>
2021-12-22 10:45:22 +0700

    add index.html file

---
+++ Circle.js
@@ -0,0 +1,16 @@
+class Circle {
+    radius;
+    color;
+
+    constructor(radius) {
+        this.radius = radius;
+    }
+
+    getRadius() {
+        return this.radius;
+    }
+
+    getArea() {
+        return this.radius * this.radius * Math.PI;
+    }
+}
